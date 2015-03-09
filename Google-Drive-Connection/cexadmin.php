1333ed994d784b216cd0b1740f778ccab0ab3924
xw805 <wxsamy@gmail.com>
2015-03-09 14:13:30 -0400

    implement convert google docs to wordpress draft

--- Google-Drive-Connection/cexadmin.php
+++ Google-Drive-Connection/cexadmin.php
@@ -6,7 +6,7 @@ function cexdrive_settings_page()
 {
     
 	$settings = cexdrive_get_config(); // Gets the current config
-    //var_dump($settings);
+    //var_dump($settings->access_token);
     $url = admin_url( '/options-general.php?page=' . basename( dirname(__FILE__) ) . '/cexdrive.php' ); // Sets the current URL
     $client = cexdrive_load_lib($url); // Initialize Google Drive Access   
     // Account authorization handler
@@ -23,7 +23,7 @@ function cexdrive_settings_page()
             $about=$service->about->get(array());
             $user=$about->getUser();
                         
-           
+           var_dump($token);
             
             // Store credentials
             if($user['emailAddress'])
@@ -35,7 +35,7 @@ function cexdrive_settings_page()
                      'user'  => $user['emailAddress'],
             );
                 //$settings = cexdrive_set_config(array($user['emailAddress'] => $data));
-                $settings=cexdrive_set_config($data);
+                $settings=cexdrive_insert_config($data);
                 $message = "User {$user['emailAddress']} added successfully.";
             }            
         }
@@ -46,57 +46,95 @@ function cexdrive_settings_page()
     }
 
         // Removing a user
-    if( isset($_GET['remove']) && $settings )
+    else if( isset($_GET['remove']) && $settings )
     {
         echo "start to remove";
         $name = $_GET['remove'];
-        //unset( $settings[ $_GET['remove'] ] );
-        $settings= cexdrive_set_config(array());
+        cexdrive_del_config();
+        $settings=FALSE;
+        //$client->revokeToken();  
         $message = "User {$name} was successfully removed.";
 
-    }  
-    
-        //check if token has been stored, if nothing is recorded, indicate first time login
-    else if ($settings)
+    } 
+
+    //check if token has been stored, if nothing is recorded, indicate first time login
+    if ($settings and !empty($settings))
     { 
         //try{
             //var_dump($settings);
             //check if token has been stored
-            if($client->isAccessTokenExpired()) {
+            //if($client->isAccessTokenExpired()) {
+            $expires_in=$settings->expires_in;
+            $created=$settings->created;
+
+            if( ( $expires_in + $created - time() ) > 0){
+                echo "retrive existing access_token";
+
+                    $token=array(
+                        'access_token'=> $settings->access_token,
+                        'expires_in' => $settings->expires_in,
+                        'created' => $settings->created
+                        );
+                    $token_json=json_encode($token);
+                    $client->setAccessToken($token_json);
+                    $service = new Google_Service_Drive($client);
+            } 
+            else{
                 echo 'Try to refresh token'; // Debug
                 //refresh token
-                if(isset($settings['token']['access_token'])){
-                //$client->refreshToken('ya29.LwE5MytwiDr3qco-YCbJr8EuJbuDmZsCtvquJKJgoQ--TRuK-f711nn2IPxpZM5-1Sx35eEzHPCbNw');
-                $client->refreshToken($settings['token']['access_token']);
-                $token_json=$client->getAccessToken();
-                $token=json_decode($token_json,true);
-                $settings['token']=$token;
-                $settings= cexdrive_set_config($settings);
-                //create a new google service
-                $client->setAccessToken($token_json);
-                $service = new Google_Service_Drive($client);
+                if(isset($settings->access_token)){
+                    //$client->refreshToken($settings->access_token);
+                    $client->refreshToken($settings->refresh_token);
+                    $token_json=$client->getAccessToken();
+                    $token=json_decode($token_json,true);
+                    $settings= cexdrive_update_config($token); 
+                    $client->setAccessToken($token_json);
+                    $service = new Google_Service_Drive($client);
+                }
+                else{
+                    echo "error: token is not set!";
+                }
+            
             }
-            else
+             //create a new google service
+                    //check if convert
+            if( isset($_GET['DocId']) )
             {
-                echo "token is not set";
-            }
-                //$about=$service->about->get(array());
-                //$user=$about->getUser();
-            }
+                echo "start to convert";
+                $fileId=$_GET['DocId'];
+                //require_once dirname(__FILE__). '/google-api-php-client/Google_Client.php';
+                try {
+                        $file = $service->files->get($fileId);
+                        print "Title: " . $file->getTitle();
+                        print "Description: " . $file->getDescription();
+                        print "MIME type: " . $file->getMimeType();
+                      } 
+                catch (Exception $e) {
+                        print "An error occurred: " . $e->getMessage();
+                      }
 
-            //the token is still valid
-            else{
+                $downloadUrl = $file->getExportLinks()['text/html'];
+                echo $downloadUrl;
 
-            }
+                if ($downloadUrl) {
+                    $request = new Google_Http_Request($downloadUrl, 'GET', null, null);
+                    //$rest=new Google_Http_REST();
+                    //$content=$rest->doExecute($client,$request);
+                    $curl=new Google_IO_Curl($client);
+                    $result=$curl->executeRequest($request);
+                    $arr["filename"]= $file->getTitle();
+                    $arr['contents']=$result[0];
 
+                }           
+                //var_dump($content);
+                $message = "Converting Document {$_GET['DocId']} ";
+                $clean_doc= get_clean_doc($arr);
+                //echo $clean_doc;
+                publish_to_WordPress($file->title,$clean_doc,false);
         
-       // }
-       // catch(exception $e)
-       // {
-       //     $error = 'The provided token is expired!';
-       // }
-
+            } 
     }
+    
        
 ?>
 <div class="wrap">
@@ -105,21 +143,27 @@ function cexdrive_settings_page()
     <div class="updated"><p><?php echo $message; ?></p></div>
 <?php elseif( isset($error) ): ?>
     <div class="error"><p><strong><?php echo $error; ?></strong></p></div>    
-<?php endif; ?>
-	
+<?php endif; ?>	
 <?php if($settings === FALSE or empty($settings) ): ?>
 	<p>You have not set up any accounts yet.</p><a href="<?php echo $client->createAuthUrl(); ?>">Add a new account</a></p>
 <?php else: ?>
 	<h3>Current Accounts:</h3>
     <ol>
-    <li><?php echo $settings['user']; ?><a href="<?php echo $url; ?>&remove=<?php echo urlencode( $settings['user']);?>">(Remove)</a></li>
+    <li><?php echo $settings->email; ?><a href="<?php echo $url; ?>&remove=<?php echo urlencode( $settings->email);?>">(Remove)</a></li>
     </ol>
     <h3> Here is the document file list on your drive</h3>
         <?php  $files_list = $service->files->listFiles(array())->getItems(); ?>
               <ol>
-                <?php foreach($files_list as $item):?> 
-                    <?php if ($item['mimeType']== "application/vnd.google-apps.folder" or $item['mimeType']=='application/vnd.google-apps.document') 
-                     echo '<li><img src="' . $item['iconLink'] . '" alt="Icon"> <a href="' . $item['embedLink'] . '" target=_self"'  . '">' . $item->title . '</a>'.'</li>'; ?>
+                 <?php foreach($files_list as $item):?> 
+                    <?php if ($item['mimeType']== "application/vnd.google-apps.folder" or $item['mimeType']=='application/vnd.google-apps.document'): 
+                     
+                     echo '<li><img src="' . $item['iconLink'] . '" alt="Icon"> <a href="' . $item['embedLink'] . '" target=_self"'  . '">' . $item->title . '</a>'.'</li>';
+                     echo "<form action=./cexdrive.php' method='get'>";
+                     echo '<input type="hidden" name="DocId" value=$item["id"]>';
+                     echo "<a href=".$url."&DocId=".$item['id'].">(convert)</a></li>";
+                     echo "<input type='submit'>";?>
+                     </form>
+                 <?php endif; ?>
                  <?php endforeach; ?>
                  </ol>
                 <br>
--- Google-Drive-Connection/cexdrive.php
+++ Google-Drive-Connection/cexdrive.php
@@ -8,36 +8,62 @@ Version: 1.2
 Author URI:
 */
 
-//start a session
-add_action('init', 'myStartSession', 1);
-add_action('wp_logout', 'myEndSession');
-add_action('wp_login', 'myEndSession');
-
-function myStartSession() {
-    if(!session_id()) {
-        session_start();
-
-    }
-}
-
-function myEndSession() {
-    session_destroy ();
-}
 // The Admin pages
 require "cexadmin.php";
 
 
 /**
- * Creates the menu page.
+ * Creates the menu item on the dashborad.
  */
-function cexdrive_create_menu() 
-{
-	// Creates a new menu under settings
-	add_options_page('Google Drive Folder Display Settings', 'Google Drive Display', 'administrator', __FILE__, 'cexdrive_settings_page');
+add_action( 'admin_menu', 'create_drive_menu' );
+        function create_drive_menu() {
+            // create menu item on the dashoboard
+                add_menu_page( 
+                'Google Drive Display', 
+                'Google Drive Display', 
+                'manage_options', 
+                __FILE__, 
+                'cexdrive_settings_page',
+                plugins_url( '/image/google_drive_icon.png', __FILE__ )
+        );
+    }
+
+
+// when plugin is activated, create a new data table in database
+register_activation_hook(__FILE__, 'plugin_activation_cretable');
+function plugin_activation_cretable() {
+    global $wpdb;
+    $table_name = $wpdb->prefix."Google_Drive_Account";
+
+    /*
+     * We'll set the default character set and collation for this table.
+     * If we don't do this, some characters could end up being converted 
+     * to just ?'s when saved in our table.
+     */
+    $charset_collate = '';
+
+    if (!empty($wpdb->charset)) {
+      $charset_collate = "DEFAULT CHARACTER SET {$wpdb->charset}";
+    }
+
+    if (!empty( $wpdb->collate)) {
+      $charset_collate .= " COLLATE {$wpdb->collate}";
+    }
+
+    $sql = "CREATE TABLE " . $table_name . " (
+        id bigint(20) NOT NULL,
+        email varchar(100) NOT NULL,
+        access_token varchar(100) NOT NULL,
+        token_type varchar(100) NOT NULL,
+        refresh_token varchar(100),
+        expires_in int(11),
+        created int (11),
+        UNIQUE KEY id (id)
+    ) $charset_collate;";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
 }
-
-add_action('admin_menu', 'cexdrive_create_menu');
-
 
 
 /**
@@ -47,14 +73,49 @@ add_action('admin_menu', 'cexdrive_create_menu');
  */
 function cexdrive_get_config()
 {
-	$settings = unserialize( get_option('cexdrive-config') );
-	if(empty($settings) || count($settings) == 0)
-	{
+
+	global $wpdb;
+	$table_name = $wpdb->prefix."Google_Drive_Account";
+	$ID=wp_get_current_user()->ID;
+	$sql='select * from '. $table_name . " where id=" .$ID;
+	$data = $wpdb->get_row($sql);
+	if (empty($data))
 		return FALSE;
-	}
-	return $settings;
+	else
+		return $data;
 }
 
+function cexdrive_update_config($token)
+{
+	global $wpdb;
+    $table_name = $wpdb->prefix."Google_Drive_Account";
+    $ID=wp_get_current_user()->ID;
+    //get current account records of user
+    $current=cexdrive_get_config();
+    if ($current)
+    {
+		$wpdb->update( 
+			$table_name, 
+			array( 
+				'access_token' =>$token['access_token'] ,	// string
+				'expires_in' => $token['expires_in'],	// integer (number) 
+				'created' => $token['created']
+			), 
+			array( 'id' => $ID ), 
+			array( 
+				'%s',	// value1
+				'%d',
+				'%d'	// value2
+			), 
+			array( '%d' ) 
+		);
+
+    }
+    //get the most update records
+    $current=cexdrive_get_config();
+
+    return $current;
+}
 
 /**
  * Sets a config option.
@@ -63,20 +124,86 @@ function cexdrive_get_config()
  * @param bool Whether to merge the data or override.
  * @return array The new data array.
  */
-function cexdrive_set_config($data)
+function cexdrive_insert_config($data)
 {
-	/*
-	$current = cexdrive_get_config();
-	if($current !== FALSE && $merge === False)
-	{
-		$data = array_merge($current, $data);
-	}
-	*/
-    update_option('cexdrive-config', serialize($data));
-	return $data;
+
+    global $wpdb;
+    $table_name = $wpdb->prefix."Google_Drive_Account";
+    //check if update or insert
+    $ID=wp_get_current_user()->ID;
+    if (! cexdrive_get_config())
+    {
+    	$rows_affected= $wpdb->insert($table_name,array( 'id' => $ID, 
+								    	'email' => $data['user'], 
+								    	'access_token' => $data['token']['access_token'],
+								    	'token_type' =>$data['token']['token_type'],
+                                        'refresh_token' => $data['token']['refresh_token'],
+								    	'expires_in' => $data['token']['expires_in'],
+								    	'created' => $data['token']['created'] ));
+
+    }
+    //get the most update records
+    $current=cexdrive_get_config();
+
+    return $current;
 }
  
 
+/**
+ * delete a config option.
+ * 
+ * @param array An array that will be merged in the existing options.
+ * @param bool Whether to merge the data or override.
+ * @return array The new data array.
+ */
+function cexdrive_del_config(){
+	global $wpdb;
+	$ID=wp_get_current_user()->ID;
+	$table_name = $wpdb->prefix."Google_Drive_Account";
+	$wpdb->query(" DELETE FROM ".$table_name." WHERE id = ".$ID);
+	//$wpdb->delete( $table_name, array( 'id' => 1 ), array( '%d' ) );
+}
+
+function get_clean_doc($contents) 
+{
+
+    $contents = apply_filters( 'pre_docs_to_wp_strip', $contents );
+    //New domDocument and xPath to get the content
+    $dom= new DOMDocument();
+    $dom->loadHTML( $contents[ 'contents' ] );
+    $xpath = new DOMXPath($dom);
+    
+    //Strip away the headers
+    $body = $xpath->query('/html/body');
+
+    
+    //This is our dirty HTML
+    $dirty_html = $dom->saveXml($body->item(0));
+    
+    $dirty_html = apply_filters( 'pre_docs_to_wp_purify', $dirty_html );
+    
+    //Run that through the purifier
+    //$clean_html = $purifier->purify( $dirty_html );
+    return $dirty_html;
+}
+
+
+function publish_to_WordPress ( $title, $content, $custom_fields = false ) {
+            //If the username in gdocs matches the username in WordPress, it will automatically apply the correct username            
+            $post_array = array(
+                'post_title' => $title,
+                'post_content' => $content,
+                'custom_fields' => $custom_fields,
+                'post_author' => wp_get_current_user()->display_name
+            );
+                   
+            //If you want all posts to be auto-published, for example, you can add a filter here
+            $post_array = apply_filters( 'pre_docs_to_wp_insert', $post_array );
+            
+            //Add
+            $post_id = wp_insert_post( $post_array );           
+    
+}
 /**
  * Initializes the Google SDK.
  * 
@@ -86,12 +213,11 @@ function cexdrive_set_config($data)
 function cexdrive_load_lib($url)
 {
 	require_once dirname(__FILE__). '/google-api-php-client/src/Google/autoload.php';
-
-	
 	$client = new Google_Client();
 	$client->setScopes(array('https://www.googleapis.com/auth/drive.readonly','https://www.googleapis.com/auth/drive'));
     $client->setRedirectUri($url);
-	
+    $client->setAccessType('offline');
+    $client->setApprovalPrompt('force');
 	return $client;
 
 }
